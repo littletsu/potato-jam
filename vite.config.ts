699cac8d0c4b8ac8ce7e5e1a26a148acb5451304
Lucia Luna <38116582+littletsu@users.noreply.github.com>
2024-08-18 00:21:40 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "https://dokxid.github.io/potato-jam/",
+    base: "https://littletsu.github.io/potato-jam/",
 })
